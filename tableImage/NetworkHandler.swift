959d49987d460d15dae8a9df3ef0d53b4acfd542
Sriram Prasad <sriramprasad@MacBook-Pro.local>
2018-11-21 21:28:12 +0530

    commit

---
+++ tableImage/NetworkHandler.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkHandler.swift
+//  tableImage
+//
+//  Created by Sriram Prasad on 21/11/18.
+//  Copyright © 2018 FullStackNet. All rights reserved.
+//
+
+import Foundation
---
+++ tableImage/TCell.swift
@@ -0,0 +1,24 @@
+//
+//  TCell.swift
+//  tableImage
+//
+//  Created by Sriram Prasad on 21/11/18.
+//  Copyright © 2018 FullStackNet. All rights reserved.
+//
+
+import UIKit
+
+class TCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
